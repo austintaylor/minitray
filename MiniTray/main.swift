107df70763110e739a2dde4b5bd91a9031fbc191
Austin Taylor <austin.taylor@gmail.com>
2014-07-31 15:32:28 -0400

    Initial Commit

---
+++ MiniTray/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  MiniTray
+//
+//  Created by Austin Taylor on 7/31/14.
+//  Copyright (c) 2014 Austin Taylor. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
---
+++ MiniTrayTests/MiniTrayTests.swift
@@ -0,0 +1,36 @@
+//
+//  MiniTrayTests.swift
+//  MiniTrayTests
+//
+//  Created by Austin Taylor on 7/31/14.
+//  Copyright (c) 2014 Austin Taylor. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class MiniTrayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
